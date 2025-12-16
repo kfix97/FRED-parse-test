e9887ddac2c04ca1baf81c71f5dde267b740d190
kfix97 <kfix97@gmail.com>
2025-12-16 01:11:57 -0500

    cleaning the original data

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 # 0) environment setup and import libraries
 # 1) construct Request URL
 # 2) send request to FRED API, get and validate response
-# 3) parse raw json response to python dicts
-# 4) convert python dicts to pandas df
+# 3) parse and clean raw json response
+# 4) load clean data to pandas df
 # 5) plot data points from pandas df
 
 # step 0: import libraries and setup environment
@@ -13,9 +13,12 @@
 # 0a) import libraries
 from dotenv import load_dotenv
 import requests
+import sys
 import pandas
 import os
 from datetime import date
+from datetime import datetime
+import pandas as pd
 
 # 0b) get environment variables from config file (API key)
 load_dotenv()
@@ -53,10 +56,81 @@ response.raise_for_status()
 # 2c) store the raw json in a variable
 raw_json = response.json()
 
-# 2d) print the get url and response for validation
-print(raw_json.keys())
+# 2d) print the column headers
+# print(raw_json.keys())
 
-json_limit = 3
+# 2e) print a preview of the response
+json_limit = 1
 observations = raw_json["observations"]
 preview = observations[:json_limit]
-print(preview)
+print("json preview: " + str(preview))
+
+# step 3: parse and clean raw json response
+
+# 3a) define the key that contains the repeating records
+observations = observations # the observations list contains the rows of data
+
+# 3b) confirm presence of the key and that the value is a list
+number_of_observations = len(observations)
+
+if number_of_observations < 1:
+    print("No rows returned in the specified dataset")
+    sys.exit()
+else:
+    print("Found some freakin' data!!")
+
+# 3c) inspect one observation row and look at the fields:
+# - date and value are the two key fields here for the x and y axes of a potential chart, respectively
+# i. drop observations with missing values in either date or value
+# ii. drop other key/value pairs besides date and value
+# iii. convert values to numeric data type and dates to a data type of date
+
+# 3d) transform raw observations to a smaller, cleaned dict with the logic above
+
+cleaned_observations = []
+
+for obs in observations:
+    # step (ii) above mentions dopping all key/value pairs besides date and value, simply focus on these two
+    raw_date = obs["date"]
+    raw_value = obs["value"]
+    
+    # FRED encodes nulls as "." rather than NULL or NaN, etc. - this is step (i) above
+    if raw_value == ".": 
+        continue
+    if raw_date == "":
+        continue
+    
+    # step (iii) above mentions data type transformations, this and the below lines accomplish that
+    parsed_date = datetime.strptime(raw_date, "%Y-%m-%d").date()
+    parsed_value = float(raw_value)
+    
+    # the cleaned dictionary/row of data to be appended to the list
+    clean_row = { 
+        "date" : parsed_date,
+        "value" : parsed_value
+    }
+    cleaned_observations.append(clean_row) # append to the list
+
+number_of_cleaned_observations = len(cleaned_observations)
+print("Of the original " + str(number_of_observations) + " data points, "
+       + str(number_of_cleaned_observations) + " passed the data validation check meaning that "+ str(number_of_observations - number_of_cleaned_observations) + " were invalid")
+
+
+# step 4: load clean data to pandas df
+
+# 4a) load data to df
+df = pd.DataFrame(cleaned_observations)
+
+# 4b) df validation checks
+# df.head()
+# df.dtypes()
+# len(df)
+
+# 4c) print sample df data
+
+
+# step 5: plot data points from pandas df
+
+# 5a) 
+
+# 5b)
