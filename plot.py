689cfc625021a5d905d19cda9ac2b1dfdcfcf2d2
kfix97 <kfix97@gmail.com>
2025-12-18 01:07:32 -0500

    plot format updates

--- plot.py
+++ plot.py
@@ -6,7 +6,12 @@ from fred_data_call import get_fred_series_df
 df = get_fred_series_df("DGS10") # series ID for the 10-yr treasury yield
 # print(df.head(2))
 
-plt.scatter(df["date"], df["value"], s=1)
+plt.scatter(
+    df["date"],
+    df["value"],
+    s=1,
+    alpha=0.2
+    )
 plt.title("10-year treasury yield over time")
 plt.xlabel("Date")
 plt.ylabel("Yield (%)")
