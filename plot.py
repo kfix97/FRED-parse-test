c7072b5e442041ba43590039addcec7b0525f761
kfix97 <kfix97@gmail.com>
2025-12-18 01:04:39 -0500

    full plot

--- plot.py
+++ plot.py
@@ -1,6 +1,13 @@
-# plot data fetched from the FRED API
+# plot data from the fred data call
 
+import matplotlib.pyplot as plt
 from fred_data_call import get_fred_series_df
 
-df = get_fred_series_df("DGS10")
-print(df.head(2))
+df = get_fred_series_df("DGS10") # series ID for the 10-yr treasury yield
+# print(df.head(2))
+
+plt.scatter(df["date"], df["value"], s=1)
+plt.title("10-year treasury yield over time")
+plt.xlabel("Date")
+plt.ylabel("Yield (%)")
+plt.show()
